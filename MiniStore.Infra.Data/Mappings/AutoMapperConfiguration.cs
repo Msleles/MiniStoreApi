c84ca4bc1f5c12fd4793cd6c7e1aaec01256c92a
unknown <maycon.leles@hubconexa.com>
2023-06-08 17:47:16 -0300

    registrando o serviço  do automapper como scoped

--- MiniStore.Infra.Data/Mappings/AutoMapperConfiguration.cs
+++ MiniStore.Infra.Data/Mappings/AutoMapperConfiguration.cs
@@ -13,7 +13,7 @@ namespace MiniStore.Infra.Data.Mappings
             });
 
             IMapper mapper = mapperConfig.CreateMapper();
-            services.AddSingleton(mapper);
+            services.AddScoped<IMapper>(provider => mapper);
         }
     }
 }
