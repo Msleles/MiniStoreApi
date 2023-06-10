e57788629fbafc8eb1c0e895d6222d6dd85bc048
unknown <maycon.leles@hubconexa.com>
2023-06-09 23:15:19 -0300

    configurando documentação do swagger

--- MiniStore.Application/DTOs/ProdutoDTO.cs
+++ MiniStore.Application/DTOs/ProdutoDTO.cs
@@ -8,6 +8,5 @@
         public string? Nome { get; set; }
         public decimal Preco { get; set; }
         public Guid CategoriaId { get; set; }
-        public string? DadosPaginacao { get; set; }
     }
 }
--- MiniStoreApi/Program.cs
+++ MiniStoreApi/Program.cs
@@ -1,8 +1,8 @@
 
+using Microsoft.OpenApi.Models;
 using MiniStore.Infra.Data.Mappings;
 using MiniStore.Infra.IoC;
-
-
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -12,14 +12,24 @@ builder.Services.AddControllers();
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mini Store API", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mini Store V1");
+    });
 }
 
 app.UseHttpsRedirection();
